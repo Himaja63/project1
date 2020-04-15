b216470425b4e97561d2fa87a27c888191bfc966
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-04-15 17:53:59 +0530

    added "/" route

--- application.py
+++ application.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/register", methods=["GET"])
 def register():
     name = request.form.get("name")
     password = request.form.get("password")
     return render_template("register.html", name=name, password=password)
 
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
